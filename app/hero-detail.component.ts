6b69a3937a1b2e633dc32ba857d39d019aa6fccc
VIJAY KONA <vijkumar8765@gmail.com>
2016-12-21 00:14:03 +0000

    first commit

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,56 @@
+
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { Hero } from './hero';
+
+import { HeroService } from './hero-service';
+
+@Component({
+	selector: 'my-hero-detail',
+	templateUrl: `app/hero-detail.component.html`
+})
+
+export class HeroDetailComponent implements OnInit {
+	
+	@Input() hero: Hero;
+	@Output() close = new EventEmitter();
+	error: any;
+	navigated = false; // true if navigated here
+
+	constructor(private heroService: HeroService, private route: ActivatedRoute){ }
+		
+	ngOnInit(){
+		this.route.params.forEach((params: Params) => {
+			if(params['id'] !== undefined){
+				let id = +params['id'];
+				this.navigated = true;
+				this.heroService.getHero(id)
+					.then(hero => this.hero = hero);
+			}else{
+				this.navigated = false;
+				this.hero = new Hero();
+			}
+		});
+	}
+	
+	goBack(savedHero: Hero = null){
+		this.close.emit(savedHero);
+		if(this.navigated){
+			window.history.back();
+		}
+		//window.history.back();
+	}
+	
+	save(){
+		this.heroService
+			.save(this.hero)
+			.then(hero => {
+				this.hero = hero;	// saved hero, w/ id if new
+				this.goBack(hero);
+			})
+			.catch(error => this.error = error); //TODO: Display error message
+	}
+	
+}
